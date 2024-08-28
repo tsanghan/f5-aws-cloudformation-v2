112a4f8d71448632aebf72f6693449bed0fae947
Wong Tsang Han <tsanghan@gmail.com>
2024-08-28 10:48:28 +0800

    remove -with-app reference in failover.yaml

---
+++ failover-existing-network/run.sh
@@ -0,0 +1,7 @@
+(for url in \
+  $(grep 'https://${S3Bucket}' failover-existing-network.yaml | \
+  sed 's/^.*\(https.*yaml\).*$/\1/' | \
+  sed -e 's/${S3Bucket}/f5-cft-v2/' \
+      -e 's/${S3Region}/us-east-1/' \
+      -e 's#${artifactLocation}#f5-aws-cloudformation-v2/v3.6.0.0/examples/#');
+  do echo $url ; done) | tee urls.txt
